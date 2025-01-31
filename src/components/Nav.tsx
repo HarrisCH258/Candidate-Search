0648f1a0048f2d53b43a45efe976804bc6d7b075
CH258 <ch2953har@gmail.com>
2025-01-31 15:21:58 -0500

    add package.json info

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div className="nav" style={{wordSpacing: "1px", justifyContent:"space-evenly", width: "15%", display:"flex", flexWrap: "wrap"}}>
+      <Link to ="/">Home</Link>
+      <Link to ="/SavedCandidates">Potential Candidates</Link>
+    </div>
   )
 };
 
